fa3efc5460e93bdb37284e02f1e00594b1f3fe67
Xing Tao Shi <xshi@stuy.edu>
2020-01-10 14:42:48 -0500

    Updates

---
+++ precache-manifest.b026f1a4309b0233c9e8c83daf152ac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e964b4266e2fe87c0ce509f8568653",
+    "url": "/ttp_assignment8/index.html"
+  },
+  {
+    "revision": "8cebcf9dd21222b873dd",
+    "url": "/ttp_assignment8/static/css/main.504b0011.chunk.css"
+  },
+  {
+    "revision": "d61129aa9a0003c3efae",
+    "url": "/ttp_assignment8/static/js/2.b3a10433.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ttp_assignment8/static/js/2.b3a10433.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8cebcf9dd21222b873dd",
+    "url": "/ttp_assignment8/static/js/main.91d5f07a.chunk.js"
+  },
+  {
+    "revision": "7a44d3ef6fdedd564089",
+    "url": "/ttp_assignment8/static/js/runtime-main.4f8839ba.js"
+  }
+]);
