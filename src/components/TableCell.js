9ccb4212ccd56e7dfa8e21f5ec59e0cc39aff954
Xing Tao Shi <xshi@stuy.edu>
2020-01-10 11:24:09 -0500

    more base features such as buttons for num rows and dropdown

--- src/components/TableCell.js
+++ src/components/TableCell.js
@@ -1,1 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export default class TableCell extends Component {
+    constructor(props) {
+	super(props);
+    }
+    render() {
+	return "Hi, I'm a cell";
+            }
+}
--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,1 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+import TableCell from './TableCell';
+
+export default class TableRow extends Component {
+    constructor(props) {
+	super(props);
+    }
+    render() {
+	return <TableCell/>
+	    }
+}
